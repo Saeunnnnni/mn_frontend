faf3dc1fde1f0051ec21bbea7507cb7d18f4dbd4
baekcoding <alsrb5055@gmail.com>
2023-08-08 16:22:23 +0900

    merge 내용 반영

--- src/App.js
+++ src/App.js
@@ -1,20 +1,13 @@
 import React from "react";
-<<<<<<< HEAD
 import SearchBar from './component/SearchBar'
-
-=======
 import CardList from "./components/CardList";
->>>>>>> upstream/lee
 
 function App() {
   return (
     <div>
-<<<<<<< HEAD
-            <SearchBar />
-=======
+      <SearchBar />
       Hello
       <CardList />
->>>>>>> upstream/lee
     </div>
   );
 }
