c26e19ebd221a736c07ae0d67a68fa80157cb686
saeunnnni <kate6011@naver.com>
2023-08-10 16:54:09 +0900

    app.js 수정

--- src/App.js
+++ src/App.js
@@ -1,20 +1,12 @@
 import React from "react";
-<<<<<<< HEAD
 import SearchBar from './component/SearchBar'
-
-=======
 import CardList from "./components/CardList";
->>>>>>> upstream/lee
 
 function App() {
   return (
     <div>
-<<<<<<< HEAD
             <SearchBar />
-=======
-      Hello
       <CardList />
->>>>>>> upstream/lee
     </div>
   );
 }
